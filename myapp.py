1c4989e001a9c0b5734a7eaf567518b07ec4c3a6
Franz Müller <36951725+francisco911@users.noreply.github.com>
2020-07-21 06:33:29 +0200

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
